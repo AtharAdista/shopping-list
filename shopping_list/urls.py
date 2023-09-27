585fbccb67eb80dae17747dc9dc58e414917839a
Athar Adista <athar0165@gmail.com>
2023-09-27 13:38:36 +0700

    finish tutorial 4

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -23,8 +23,4 @@ from main.views import show_main, create_product, show_xml, show_json, show_xml_
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'),
-    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
 ]
